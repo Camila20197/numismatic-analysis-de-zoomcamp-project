4801253236da47b07bc34943324ee668490bf814
Camila20197 <camiladurand2097@gmail.com>
2026-04-02 23:54:44 -0300

    Correciones en upoad_data.py

--- src/gc_utility/upload_data.py
+++ src/gc_utility/upload_data.py
@@ -4,16 +4,13 @@ from prefect_gcp.cloud_storage import GcsBucket
 
 @task()
 async def upload_csv_to_gcs(bucket_name: str, source_file_path: str, gcs_key: str):
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(gcs_key)
+    blob.upload_from_filename(source_file_path)
     
-    # old way before using prefect
-
-    # storage_client = storage.Client()
-    # bucket = storage_client.bucket(bucket_name)
-    # blob = bucket.blob(gcs_key)
-    # blob.upload_from_filename(source_file_path)
-    
-    gcs_bucket_block = await GcsBucket.load(bucket_name)
-    await gcs_bucket_block.upload_from_path(from_path=source_file_path, to_path=gcs_key)
-    print(f"File {source_file_path} uploaded")
+    # gcs_bucket_block = await GcsBucket.load(bucket_name)
+    # await gcs_bucket_block.upload_from_path(from_path=source_file_path, to_path=gcs_key)
+    # print(f"File {source_file_path} uploaded")
 
 
